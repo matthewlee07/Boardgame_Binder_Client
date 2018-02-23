8ae968a8672852ef292d8746ed933ef8f10ece69
matthew lee <matthewlee07@gmail.com>
2018-02-23 09:01:13 -0500

    user login

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from '../userModel';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent implements OnInit {
-
+export class UserLoginComponent {
   constructor() { }
-
-  ngOnInit() {
+  user: User = new User();
+  submitted = false;
+  forgotPassword() {
+    console.log('resending password');
   }
-
+  onSubmit() { this.submitted = true; }
 }
