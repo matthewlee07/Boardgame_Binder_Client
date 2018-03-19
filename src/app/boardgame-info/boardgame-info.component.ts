5cd7fbaa98d1804958479abb1866be4608f7d164
matthew lee <matthewlee07@gmail.com>
2018-03-19 16:52:53 -0400

    replaced boardgameinfocomponent with old working code, commented in are nonworking code to be implemented

--- src/app/boardgame-info/boardgame-info.component.ts
+++ src/app/boardgame-info/boardgame-info.component.ts
@@ -1,24 +1,28 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
+// import { Router } from '@angular/router';
 import { Boardgame } from '../boardgameModel';
-import { User } from '../userModel';
-import { UserService } from '../user.service';
-import { UserBoardgames } from '../user-boardgames-Model';
-import { UserBoardgamesService } from '../user-boardgames.service';
+// import { User } from '../userModel';
+// import { UserService } from '../user.service';
+// import { UserBoardgames } from '../user-boardgames-Model';
+// import { UserBoardgamesService } from '../user-boardgames.service';
 
 @Component({
   selector: 'app-boardgame-info',
   templateUrl: './boardgame-info.component.html',
   styleUrls: ['./boardgame-info.component.css']
 })
-export class BoardgameInfoComponent {
-  constructor(private userservice: UserService, private userboardgames: UserBoardgames, private router: Router) { }
-  // user: User = new User();
+export class BoardgameInfoComponent implements OnInit {
+  // export class BoardgameInfoComponent {
   showDetails = false;
   @Input() game: Boardgame;
+  constructor() { }
+  // constructor(private userservice: UserService, private userboardgames: UserBoardgames, private router: Router) { }
+  // user: User = new User();
+  // addGame() {
+  //   if !user.isLoggedIn() { this.router.navigateByUrl('/userLogin')}
+  // }
 
-  addGame() {
-// if !user.isLoggedIn() { this.router.navigateByUrl('/userLogin')}
-//
+  ngOnInit() {
   }
 }
