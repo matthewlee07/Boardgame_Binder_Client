1f94659eb5461bde46fd41f7ec6e17b15b6bdd1a
matthew lee <matthewlee07@gmail.com>
2018-04-02 15:39:05 -0400

    unfinished remove/edit from userboardgames

--- src/app/user-boardgames.service.ts
+++ src/app/user-boardgames.service.ts
@@ -55,7 +55,7 @@ export class UserBoardgamesService {
 
   // }
 
-deleteGame(user, gameID) {
+  deleteGame(user, gameID) {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
