ba877388bfda4679497fc4d6b02122258f0a9cf2
matthew lee <matthewlee07@gmail.com>
2018-05-04 23:03:18 -0400

    meh

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/boardgames'));
+app.use(express.static(__dirname + '/dist/'));
 
-app.get('/*a', function (req, res) {
+app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/boardgames'));
+    res.sendFile(path.join(__dirname + '/dist/'));
 });
 
 app.listen(process.env.PORT || 8080);
