d819f5b317c2985987247f2ab46d4639b4b971e8
matthew lee <matthewlee07@gmail.com>
2018-05-04 23:08:44 -0400

    meh

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/boardgames'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/boardgames'));
+    res.sendFile(path.join(__dirname + '/dist/boardgames.index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
