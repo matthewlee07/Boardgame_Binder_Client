41d0d8eacc3582e7c060c2d9b22213064a08b8c9
matthew lee <matthewlee07@gmail.com>
2018-05-04 23:17:19 -0400

    meh

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/boardgame-binder'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/boardgame-binder.index.html'));
+    res.sendFile(path.join(__dirname + '/dist/boardgame-binder/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
